19b609927d842ab78ca30d2f0b6dfe3a038f4c00
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2020-06-01 09:11:58 +0200

    Make some constants constants in myrand.c.

--- myrand.c
+++ myrand.c
@@ -1,16 +1,18 @@
 #include "spsa_sim.h"
 
+static const double pow2_64=0x1.0000000000000p+64;
+static const uint64_t a=UINT64_C(2862933555777941757);
+static const uint64_t b=UINT64_C(3037000493);
+static const uint64_t a48=UINT64_C(3311271626024157185);
+static const uint64_t b48=UINT64_C(8774982398954700800);
+
 double myrand(uint64_t *prng){
-  uint64_t a=UINT64_C(2862933555777941757);
-  uint64_t b=UINT64_C(3037000493);
   uint64_t current=*prng;
   *prng=a*(*prng)+b;
-  return current/pow(2,64);
+  return current/pow2_64;
 }
 
 void jump(uint64_t *prng){
   /* do 2^48 steps */
-  uint64_t a=UINT64_C(3311271626024157185);
-  uint64_t b=UINT64_C(8774982398954700800);
-  *prng=a*(*prng)+b;
+  *prng=a48*(*prng)+b48;
 }
