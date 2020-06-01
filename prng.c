e102cb64e7df9f5433b040e8f893b75dce176b85
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2020-06-01 09:51:57 +0200

    More professional seed generation.

--- prng.c
+++ prng.c
@@ -1,10 +1,21 @@
+#include <time.h>
+#include <assert.h>
+
 #include "prng.h"
 
-static const double pow2_64=0x1.0000000000000p+64;
-static const uint64_t a=UINT64_C(2862933555777941757);
-static const uint64_t b=UINT64_C(3037000493);
-static const uint64_t a48=UINT64_C(3311271626024157185);
-static const uint64_t b48=UINT64_C(8774982398954700800);
+static const double   pow2_64 = 0x1.0000000000000p+64;
+static const uint64_t pow10_9 = UINT64_C(1000000000);
+static const uint64_t a       = UINT64_C(2862933555777941757);
+static const uint64_t b       = UINT64_C(3037000493);
+static const uint64_t a48     = UINT64_C(3311271626024157185);
+static const uint64_t b48     = UINT64_C(8774982398954700800);
+
+prng_t prng_seed(void){
+  struct timespec t;
+  int ret=clock_gettime(CLOCK_REALTIME,&t);
+  assert(ret==0);
+  return pow10_9*((uint64_t) t.tv_sec)+((uint64_t) t.tv_nsec);
+}
 
 double prng_get(prng_t *prng){
 /*
