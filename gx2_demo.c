7ae2dc653c7603f4370cf5fc5209c903448c0efe
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2020-06-07 15:35:38 +0200

    A few more examples.

--- gx2_demo.c
+++ gx2_demo.c
@@ -3,15 +3,25 @@
 #include "gx2.h"
 
 int main(){
-  double val     =0.148139684865550; /* MATLAB value */
   double nt      =3;
-  double x       =25;
   double coeffs[]={1,5,2};
   int    df[]    ={1,2,3};
   double lambda[]={2,3,7};
   gx2_stats_t stats;
-  double p=gx2cdf(nt,x,coeffs,df,lambda,&stats);
+
+  double x,x0,p,p0;
+
+  x=25;
+  double val=0.148139684865550; /* MATLAB value */
+
+  p=gx2cdf(nt,x,coeffs,df,lambda,&stats);
   printf("p=%16.16g dev=%g exit=%d chi2_calls=%d err=%g\n",p,p-val,stats.error_num,stats.chi2_calls,stats.truncation_error);
-  double x0=gx2ppf(nt,p,coeffs,df,lambda,&stats);
+  x0=gx2ppf(nt,p,coeffs,df,lambda,&stats);
   printf("x=%16.16g dev=%g exit=%d iterations=%d chi2_calls=%d funcalls=%d\n",x0,x0-x,stats.error_num,stats.iterations,stats.chi2_calls, stats.funcalls);
+
+  p=0.65;
+  x=gx2ppf(nt,p,coeffs,df,lambda,&stats);
+  printf("x=%16.16g exit=%d iterations=%d chi2_calls=%d funcalls=%d\n",x,stats.error_num,stats.iterations,stats.chi2_calls, stats.funcalls);
+  p0=gx2cdf(nt,x,coeffs,df,lambda,&stats);
+  printf("p=%16.16g dev=%g exit=%d chi2_calls=%d err=%g\n",p0,p0-p,stats.error_num,stats.chi2_calls,stats.truncation_error);
 }
--- sos_demo.c
+++ sos_demo.c
@@ -1,8 +1,10 @@
 #include "spsa_sim.h"
 
 int main(){
-  sos_t sos={4,{1,1,1,1},{0,0,0,0},{1,1,1,1}};
+  sos_t sos={8,{1,1,1,1,1,1,1,1},{0,0,0,0,0,0,0,0},{1,1,1,1,1,1,1,1}};
   double x=sos_ppf(&sos, 0.95);
-  printf("%10.10f\n",x);
-  printf("%10.10f\n",sos_cdf(&sos, x));
+  double chi2=gsl_cdf_chisq_P(x,8);
+  double p=sos_cdf(&sos, x);
+  printf("p_input=%17.17g\n",0.95);
+  printf("x=%17.17g p=%17.17g chi2=%17.17g dev=%g\n",x,p,chi2,p-chi2);
 }
